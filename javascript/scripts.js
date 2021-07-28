111c77ba96d2fc43a5e5c2c8948ec4b61b19c7d4
manfred-commits <manfredipiraino@gmail.com>
2021-07-28 13:36:58 +0200

    added prompt request of all the variables

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,6 +1,8 @@
 // richiesta nome
-var name= parseInt(prompt("Inserisci il tuo nome:"))
+var name= parseInt(prompt("Inserisci il tuo nome:"));
 // richiesta cognome
+var surname= parseInt(prompt("Inserisci il tuo cognome:"));
 // richiesta colore preferito
+var color= parseInt(prompt("Inserisci il tuo colore preferito:"));
 // inserimento dei valori ottenuti in un unica variabile
 // inserimento contenuto della variabile contenente tutto all'interno del contenuto del tag html
