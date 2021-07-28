f099bb0a428bc56ea9b8d8d3d7b014de0bb7169e
manfred-commits <manfredipiraino@gmail.com>
2021-07-28 13:35:32 +0200

    project start

---
+++ javascript/scripts.js
@@ -0,0 +1,6 @@
+// richiesta nome
+var name= parseInt(prompt("Inserisci il tuo nome:"))
+// richiesta cognome
+// richiesta colore preferito
+// inserimento dei valori ottenuti in un unica variabile
+// inserimento contenuto della variabile contenente tutto all'interno del contenuto del tag html
