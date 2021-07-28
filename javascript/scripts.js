21b7c2e91d692e3a858b3a2230cf518ec83c5bf3
manfred-commits <manfredipiraino@gmail.com>
2021-07-28 14:10:32 +0200

    added fix to the var name "name" changed to "nome"

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,10 +1,10 @@
 // richiesta nome
-var name= (prompt("Inserisci il tuo nome:"));
+var nome= (prompt("Inserisci il tuo nome:"));
 // richiesta cognome
 var surname= (prompt("Inserisci il tuo cognome:"));
 // richiesta colore preferito
 var color= (prompt("Inserisci il tuo colore preferito:"));
 // inserimento dei valori ottenuti in un unica variabile
-var passwordGenerated = name + surname + color + "40";
+var passwordGenerated = nome + surname + color + "40";
 // inserimento contenuto della variabile contenente tutto all'interno del contenuto del tag html
 document.getElementById("password").innerHTML += passwordGenerated;
