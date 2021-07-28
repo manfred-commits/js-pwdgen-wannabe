4f4832e3936b075cf67f277088d7a94a85c0218e
manfred-commits <manfredipiraino@gmail.com>
2021-07-28 14:30:40 +0200

    removed unnecessary parenthesis in var property

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,9 +1,9 @@
 // richiesta nome
-var nome= (prompt("Inserisci il tuo nome:"));
+var nome= prompt("Inserisci il tuo nome:");
 // richiesta cognome
-var surname= (prompt("Inserisci il tuo cognome:"));
+var surname= prompt("Inserisci il tuo cognome:");
 // richiesta colore preferito
-var color= (prompt("Inserisci il tuo colore preferito:"));
+var color= prompt("Inserisci il tuo colore preferito:");
 // inserimento dei valori ottenuti in un unica variabile
 var passwordGenerated = nome + surname + color + "40";
 // inserimento contenuto della variabile contenente tutto all'interno del contenuto del tag html
