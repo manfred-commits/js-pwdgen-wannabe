4626b8c1c954039c49562dc2391e46bb3ebf20b2
manfred-commits <manfredipiraino@gmail.com>
2021-07-28 13:38:47 +0200

    added a check, to see the content in password

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -5,4 +5,6 @@ var surname= parseInt(prompt("Inserisci il tuo cognome:"));
 // richiesta colore preferito
 var color= parseInt(prompt("Inserisci il tuo colore preferito:"));
 // inserimento dei valori ottenuti in un unica variabile
+var password = name + surname + color + "40";
+console.log(password);
 // inserimento contenuto della variabile contenente tutto all'interno del contenuto del tag html
